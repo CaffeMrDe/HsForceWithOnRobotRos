f04861eb420777ad09344ef4a446a87d3b586264
K <13674059697@163.com>
2020-06-17 09:44:59 +0800

    添加通讯开关接口

--- daq_ros_driver/src/driver.cpp
+++ daq_ros_driver/src/driver.cpp
@@ -17,50 +17,102 @@
 #include "geometry_msgs/WrenchStamped.h"
 #include "hirop_msgs/getOnRobotForce.h"
 #include "hirop_msgs/setOnRobotForce.h"
+#include <thread>
+#include "unistd.h"
+#include "csignal"
 #define IFRAME "eno1"
 #define DEBUG
 
 using namespace std;
-bool flag = false;
+bool running = false;
+float *buffer;
+ros::Publisher state_pub;
+geometry_msgs::Wrench tmp;
+//#define ETHERCAT_FUNCTION
 
-void runOnRobotEthercatServer(hirop_msgs::setOnRobotForceRequest& req,hirop_msgs::setOnRobotForceResponse& res){
+boost::shared_ptr<ros::NodeHandle> n;
+void runOnRobotThread()
+{
+    int ret = -1;
+    ros::Rate loop_rate(200);
+    ROS_INFO_STREAM("runOnRobotThread running...");
+    while(ros::ok() && running){
 
+#ifdef ETHERCAT_FUNCTION
+        ec_send_processdata();
+        ret = ec_receive_processdata(EC_TIMEOUTRET);
+//        cout << "ec_receive_processdata ret = " << ret << endl;
+        buffer = (float *)ec_slave[1].inputs;
+    #ifdef DEBUG
+        cout << "Fx=" << buffer[0] << " || Fy= " << buffer[1] << " || Fz=" << buffer[2] << " || Tx=" << buffer[3] << " || Ty=" << buffer[4] << " || Tz=" << buffer[5] << endl;
+    #endif
+        tmp.force.x = buffer[0];tmp.force.y = buffer[1];tmp.force.z = buffer[2];
+        tmp.torque.x = buffer[3];tmp.torque.y = buffer[4];tmp.torque.z = buffer[5];
+#else
+       tmp.force.x = 0; tmp.force.y = 0;tmp.force.z = 0;
+       tmp.torque.x = 0;tmp.torque.y = 0;tmp.torque.z = 0;
+#endif
+        state_pub.publish(tmp);
+
+        loop_rate.sleep();
+    }
+    ROS_INFO_STREAM("runOnRobotThread exit...");
+
+}
+
+void signalHandler(int signum)
+{
+  ROS_INFO("%s is received, Terminating the node...",strsignal(signum));
+//  n->shutdown();
+  exit(signum);
+}
+
+bool OnRobotEthercatServer(hirop_msgs::setOnRobotForceRequest& req,hirop_msgs::setOnRobotForceResponse& res){
+    if(req.enable && running == false)
+    {
+        running = true;
+        std::thread t1(runOnRobotThread);
+        t1.detach();
+
+    }else if(running == true && (!req.enable))
+    {
+        running = false;
+    }
+    return true;
 }
 
 int main(int argc, char **argv){
 
 	int ret;
-	float *buffer;
 	char IOmap[4096];
 	int chk;
 
-
-	ros::init(argc, argv, "daq_driver");
-	ros::NodeHandle n;
-	ros::Publisher state_pub;
-
-
-	ret = ec_init(IFRAME);		// 初始化soem
+    ros::init(argc, argv, "onRobot_daq_driver");
+//    ros::NodeHandle n;
+    n = boost::make_shared<ros::NodeHandle>();
+#ifdef ETHERCAT_FUNCTION
+    // 初始化soem
+    ret = ec_init(IFRAME);
 	if(ret < 0){
-		cout << "ec_init error, ret = " << ret << endl;
+        ROS_INFO_STREAM("ec_init error, ret = " << ret );
 		return -1;
 	}	
 
 	ret = ec_config_init(FALSE);	// 初始化并配置
 	if(ret < 0){
-		cout << "ec_config_init error, ret = " << ret << endl;
+        ROS_INFO_STREAM("ec_config_init error, ret = " << ret );
 		return -1;	
 	}
 
-	cout << "ec_slavecount = " << ec_slavecount << endl;
+    ROS_INFO_STREAM("ec_slavecount = " << ec_slavecount );
 	if(ec_slavecount > 0)
-		cout << "ec_slave0 name was: " << ec_slave[1].name << endl;
+        ROS_INFO_STREAM("ec_slave0 name was: " << ec_slave[1].name );
 
 	ec_config_map(&IOmap);
 	ec_configdc();
 
 	ec_statecheck(0, EC_STATE_SAFE_OP,  EC_TIMEOUTSTATE);	// 等待所有从站切换至safe OP状态
-	cout << "all slave state was safe op" << endl;
+    ROS_INFO_STREAM("all slave state was safe op" );
 
 	ec_slave[0].state = EC_STATE_OPERATIONAL;
     ec_send_processdata();
@@ -76,39 +128,22 @@ int main(int argc, char **argv){
     }
     while (chk-- && (ec_slave[0].state != EC_STATE_OPERATIONAL)); // 等待所有从站切换至OP状态
 
-	cout << "all slave state was op" << endl;
+    ROS_INFO_STREAM("all slave state was op " );
 
-#ifdef DEBUG
-	cout << "+++++++++++++++++++++" << chk << endl;
-	cout << "Obits" << ec_slave[1].Ibits << endl;
+    ROS_INFO_STREAM("---------------------- " << chk);
+    ROS_INFO_STREAM("Obits" << ec_slave[1].Ibits );
 #endif
 
 
-    ros::Rate loop_rate(200);
-	state_pub = n.advertise<geometry_msgs::Wrench>("daq_data", 1000);
-	geometry_msgs::Wrench tmp; 
+    state_pub = n->advertise<geometry_msgs::Wrench>("daq_data", 1000);
 
-
-//    ros::ServiceServer service = n.advertiseService("add_two_ints",add);
-
-    ros::spinOnce();
+    ros::ServiceServer service = n->advertiseService("set_onRobot_force_enable",OnRobotEthercatServer);
+    ROS_INFO_STREAM( "onRobot_daq_driver start ....");
+    signal(SIGINT,signalHandler);
+    ros::spin();
 
 	return 0;
 }
 
 
-//while(ros::ok()){
 
-//    ec_send_processdata();
-//    ret = ec_receive_processdata(EC_TIMEOUTRET);
-//    cout << "ec_receive_processdata ret = " << ret << endl;
-//    buffer = (float *)ec_slave[1].inputs;
-//#ifdef DEBUG
-//    cout << "Fx=" << buffer[0] << " || Fy= " << buffer[1] << " || Fz=" << buffer[2] << " || Tx=" << buffer[3] << " || Ty=" << buffer[4] << " || Tz=" << buffer[5] << endl;
-//#endif
-//    tmp.force.x = buffer[0];tmp.force.y = buffer[1];tmp.force.z = buffer[2];
-//    tmp.torque.x = buffer[3];tmp.torque.y = buffer[4];tmp.torque.z = buffer[5];
-//    state_pub.publish(tmp);
-
-//    loop_rate.sleep();
-//}
